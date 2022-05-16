103e4699631b99938607859d7b8372161967c19f
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-16 21:08:58 +0200

    feat: user model

---
+++ src/models/User.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const jwt = require('jsonwebtoken');
+Joi.objectId = require('joi-objectid')(Joi);
+
+const UserSchema = new mongoose.Schema({
+    fname: {
+        type: String,
+        required: true,
+        minlength: 3,
+
+    },
+    lname: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    usernames: {
+        type: String,
+
+    },
+    phone: {
+        type: String,
+
+    }
+}, {
+    timestamps: true,
+})
+
+exports.User = mongoose.model("User", UserSchema);
+
+exports.validateUser = function (user) {
+    const schema = Joi.object({
+        fname: Joi.string().min(3).required(),
+        lname: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(3).required(),
+        isAdmin: Joi.boolean(),
+        usernames: Joi.string(),
+        phone: Joi.string(),
+    })
+    return schema.validate(user);
+}
