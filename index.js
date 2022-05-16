41c361b0ceda99e87eae1aa935a2e6005db5d2eb
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-16 20:59:39 +0200

    feat: starter

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const port = process.env.PORT || 5000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use("cors", cors());
+
+app.set('port', port);
+
+mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running  https://localhost/${port}`);
+        });
+    }).catch(err => {
+        console.log(err);
+        process.exit()
+    })
+
+
+app.get('/', (req, res) => {
+    res.json("Welcome to the API");
+})
+
