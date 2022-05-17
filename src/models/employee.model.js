cce355ceac2f38a722ee60063a5dda9661abf349
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-17 22:16:20 +0200

    feat: add protected routes

--- src/models/employee.model.js
+++ src/models/employee.model.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectId')(Joi);
-const { User } = require("../models/user.model");
 const pagination = require("mongoose-paginate-v2");
 var depopulate = require('mongoose-deep-populate')(mongoose);
 const { EDepartments } = require("../utils/common.util");
@@ -20,7 +19,6 @@ const EmployeeSchema = mongoose.Schema({
 }, {
     timestamps: true
 })
-
 
 EmployeeSchema.plugin(pagination);
 EmployeeSchema.plugin(depopulate);
