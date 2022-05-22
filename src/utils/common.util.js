1c8e40d9f17dd4522f32b0267c3f6f6a2bde7057
Shallon Kobusinge <kobshallon041@gmail.com>
2022-05-22 12:27:57 +0200

    feat: Modify enum for departments

--- src/utils/common.util.js
+++ src/utils/common.util.js
@@ -1,22 +1,8 @@
-// enum EDepartments {
-//     IT = 'IT',
-//     HR = 'HR',
-//     Finance = 'Finance',
-//     Sales = 'Sales',
-//     Marketing = 'Marketing'
-// }
 const EDepartments = Object.freeze({
     "IT": "IT",
     "HR": "HR",
     "Finance": "Finance",
 
-    // IT: 'IT',
-    // HR: 'HR',
-    // Finance: 'Finance',
-    // Sales: 'Sales',
-    // Marketing: 'Marketing'
-})
-
-module.exports = {
-    EDepartments
-}
+    module.exports = {
+        EDepartments
+    }
